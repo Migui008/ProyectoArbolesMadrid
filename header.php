9a2e37944fade7c2a91a3d0e301ea075251c6d56
Migui008 <59126015+Migui008@users.noreply.github.com>
2024-05-29 18:32:33 +0200

    Update header.php

--- header.php
+++ header.php
@@ -1,10 +1,10 @@
 <div class="menubar">
-    <div class="sectionicon"><a href="mainmenu"><img src="icono.png" alt="icono"></a></div>
+    <div class="sectionicon"><a href="index"><img src="icono.png" alt="icono"></a></div>
     <div class="sectionfilter">
-      <div class="navarboles navbutton"><a href="/arboles.php">Arboles</a></div>
-      <div class="navparques navbutton"><a href="/parques.php">Parques</a></div>
+      <div class="arboles navbutton"><a href="/arboles">Arboles</a></div>
+      <div class="parques navbutton"><a href="/parques">Parques</a></div>
     </div>
     <div class="sectionlogin">
-      <button id="loginbutton" class="loginbutton">Login</button>
+      <button class="loginbutton">Login</button>
     </div>
   </div>
