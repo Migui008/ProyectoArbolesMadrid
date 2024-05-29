2bdb39d9235111665e1aecd0b6659867fc155b25
Migui008 <59126015+Migui008@users.noreply.github.com>
2024-05-29 18:31:24 +0200

    Update parques.php

--- parques.php
+++ parques.php
@@ -4,14 +4,39 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="styles.css">
 </head>
 <body>
 <?php
-  session_start();
-  require_once("header.php");
-  require_once("sidebar.php");
-  require_once("functions.php");
-  ?>
+session_start();
+require_once("header.php");
+require_once("functions.php");
+?>
+<div class="container">
+<?php require_once("sidebar.php"); ?>
+<div class="mainContent">
+    <h1>Parques</h1>
+    <div>
+    <?php
+    echo "1<br>"; // Depuración
+    if (!isset($_SESSION['parquesAllNames']) || $_SESSION['parquesAllNames'] == []) {
+        echo "2<br>"; // Depuración
+        getAllParques();
+        echo "3<br>"; // Depuración
+    } else {
+        echo "Parques ya cargados en la sesión.<br>"; // Depuración
+    }
+    if (isset($_SESSION['parquesAllNames'])) {
+        echo "<pre>";
+        print_r($_SESSION['parquesAllNames']); // Depuración
+        echo "</pre>";
+    } else {
+        echo "No hay parques disponibles en la sesión.<br>"; // Depuración
+    }
+    ?>
+    </div>
+</div>
+</div>
 </body>
-<script src="./script.js"></script>
+<script src="./functions.js"></script>
 </html>
